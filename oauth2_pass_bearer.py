59c1760054e07b319fa6cac951c23aa0785766ca
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-10-17 09:18:19 +0900

    simple-oauth2

--- oauth2_pass_bearer.py
+++ oauth2_pass_bearer.py
@@ -1,6 +1,6 @@
 from typing import Annotated, Optional
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
@@ -32,6 +32,24 @@ def get_password_hash(password):
     return "fakehashed" + password
 
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user_dict = fake_users_db.get(token)
+    if user_dict:
+        return User(**user_dict)
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="Invalid authentication credentials",
+                        headers={"WWW-Authenticate": "Bearer"})
+
+
+def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
 @app.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_dict = fake_users_db.get(form_data.username)
@@ -44,3 +62,9 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
         raise HTTPException(
             status_code=400, detail="Incorrect username or password")
     return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/user/me")
+async def read_user_me(
+        current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
